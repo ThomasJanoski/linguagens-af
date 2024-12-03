1f4f56eb6b2d93eb0f9a405c369757b2e4e357aa
ThomasJanoski <thomassilveira2020@gmail.com>
2024-12-03 19:41:21 -0300

    Atualização

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function somar() {
+    const quantidade = 6;
+    let valorTotal = 0;
+
+    for (let i = 0; i < quantidade; i++) {
+        valorTotal = valorTotal + 0.30;
+    }
+
+    console.log(valorTotal + 0.30);
+}
+
+somar();
+
+console.log(2 + "2" - 1);
